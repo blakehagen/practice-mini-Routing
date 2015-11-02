7872de37afd5dd0db20525bb242ee57b7f428a41
Blake Hagen <blakehagen@gmail.com>
2015-11-01 20:29:06 -0700

    routing setup

--- js/app.js
+++ js/app.js
@@ -1,2 +1,27 @@
-angular.module('miniRouting', ['ui.router']);
+angular.module('miniRouting', ['ui.router']).config(function ($stateProvider, $urlRouterProvider) {
+
+    $stateProvider
+        .state('home', {
+            url: '/',
+            templateUrl: 'js/home/homeTmpl.html',
+            controller: 'homeCtrl'
+        })
+        .state('settings', {
+            url: '/settings',
+            templateUrl: 'js/settings/settingsTempl.html',
+            controller: 'settingsCtrl'
+        })
+        .state('products', {
+            url: '/products/:id',
+            templateUrl: 'js/products/productTmpl.html',
+            controller: 'productsCtrl'
+        });
+
+    $urlRouterProvider
+        .otherwise('/');
+
+
+
+
+});
 
